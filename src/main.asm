f02a48e829bf041716c22a42af14446032906809
davidSimek <david.simek@infis.cz>
2023-10-07 20:09:29 +0200

    updated structure to make it more easy to read

---
+++ src/main.asm
@@ -0,0 +1,21 @@
+format ELF64 executable 3
+
+segment readable executable
+
+entry main
+include "includes/io.inc"
+include "includes/run.inc"
+include "includes/data.inc"
+include "includes/stack.inc"
+
+main:
+    printD namePrompt
+    readD buffer
+    printD buffer
+
+    exit 0
+
+segment readable writable
+
+strD namePrompt, "give me your name: ", 10
+resD buffer, 20
